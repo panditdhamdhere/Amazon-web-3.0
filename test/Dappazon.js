67074b94b5bc55f9a14b0e7b6229efbd6be055f4
panditdhamdhere <dhamdherepandit@gmail.com>
2023-04-11 21:57:25 +0530

    first commit

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,21 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
+  let dappazon;
 
-})
+  beforeEach(async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", () => {
+    it("has a name", async () => {
+      const name = await dappazon.name();
+      expect(name).to.equal("Dappazon");
+    });
+  });
+});
